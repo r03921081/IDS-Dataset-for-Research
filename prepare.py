cf1b9b07e9d673f057061d5ed6d984c9ba4c20d4
r03921081 <a98703003@gmail.com>
2016-06-21 07:17:25 +0000

    prepare

--- prepare.py
+++ prepare.py
@@ -1,14 +1,17 @@
-import pandas
+import pandas as pd
+import numpy as np
 from sklearn import svm
+#raw_data = pd.read_csv("unsw/UNSW_NB15_training-set.csv", delimiter=',', encoding="utf-8-sig")
+raw_data = pd.read_csv("unsw/UNSW_Discretize.csv", delimiter=',', encoding="utf-8-sig")
+print(raw_data.columns)
+print(raw_data.shape)
+print(raw_data)
+#print(data[(data['proto']=='tcp') and (data['service']=='http')])
+#print(data[data['service']=='http'])
+#print(data['attack_cat'])
+#print(data.corr()['label'])
+#svc = svm.SVC(kernel='linear')
 
-data = pandas.read_csv("unsw/UNSW_NB15_training-set.csv", delimiter=',', index_col="id", encoding="utf-8-sig")
-
-print(data.columns)
-print(data.shape)
-
-print(data['attack_cat'])
-
-print(data.corr()['label'])
-
-svc = svm.SVC(kernel='linear')
-
+#new_data = data.drop(['id', 'proto', 'service', 'state'], axis=1)
+#print(new_data.shape)
+#print(new_data)
