84baf2339f572055ae728d844a71624aea4814a0
r03921081 <a98703003@gmail.com>
2016-06-21 08:56:32 +0000

    0456

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn import metrics
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction import DictVectorizer as dv
 
 raw_data = pd.read_csv("unsw/UNSW_NB15_training-set.csv", delimiter=',', encoding="utf-8-sig")
 #raw_data = pd.read_csv("unsw/UNSW_Discretize.csv", delimiter=',', encoding="utf-8-sig")
@@ -15,31 +15,32 @@ print(raw_data.shape)
 #print(raw_data['label'])
 #print(data.corr()['label'])
 
+old_data = raw_data.drop(['id', 'attack_cat', 'label'], axis=1)
+print(old_data)
+pre_data = old_data.T.to_dict().values()
+vectorizer = dv(sparse = False)
+dv_data = vectorizer.fit_transform(pre_data)
+print(dv_data)
+
+#encode_data = pd.get_dummies(raw_data['state'])
+#print(encode_data)
+
 new_data = raw_data.drop(['id', 'proto', 'service', 'state', 'attack_cat', 'label'], axis=1)
 new_target = raw_data['label']
 #print(new_data.shape)
 #print(new_data)
 
 np_data = new_data.as_matrix()
+print(np_data)
 np_target = new_target.as_matrix()
 
 model = GaussianNB()
-model.fit(np_data, np_target)
+model.fit(dv_data, np_target)
 print(model)
 
 expected = np_target
-predicted = model.predict(np_data)
+predicted = model.predict(dv_data)
 
 print(metrics.classification_report(expected, predicted))
 print(metrics.confusion_matrix(expected, predicted))
 
-model = DecisionTreeClassifier()
-model.fit(np_data, np_target)
-print(model)
-
-expected = np_target
-predicted = model.predict(np_data)
-
-print(metrics.classification_report(expected, predicted))
-print(metrics.confusion_matrix(expected, predicted))
-print(metrics.precision_score(expected, predicted))
