5dfe90ab72b9cecaef4362887e7cdfefa08f431e
r03921081 <a98703003@gmail.com>
2016-06-23 06:40:15 +0000

    Test

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+import sys
+
+from itertools import chain, combinations
+from collections import defaultdict
+from optparse import OptionParser
+
 import pandas as pd
 import numpy as np
 from sklearn import metrics
@@ -11,20 +17,161 @@ proto_table = {"udp": 1, "arp": 2, "tcp": 3, "igmp": 4, "ospf": 5, "sctp": 6, "g
 service_table = {"-": 1, "http": 2, "ftp": 3, "ftp-data": 4, "smtp": 5, "pop3": 6, "dns": 7, "snmp": 8, "ssl": 9, "dhcp": 10, "irc": 11, "radius": 12, "ssh": 13}
 state_table = {"INT": 1, "FIN": 2, "REQ": 3, "ACC": 4, "CON": 5, "RST": 6, "CLO": 7}
 
+
+# TRAINING DATA
 raw_train_data = pd.read_csv("unsw/UNSW_NB15_training-set.csv", delimiter=',', encoding="utf-8-sig")
 print(raw_train_data.columns)
 print(raw_train_data.shape)
 
-train_data = raw_train_data.drop(['id','proto', 'state', 'service', 'attack_cat', 'label'], axis=1)
+copy_train = raw_train_data.drop(['id', 'attack_cat', 'label'], axis=1)
+train_target = raw_train_data['label']
+cat_train = pd.DataFrame(index = copy_train.index, columns = copy_train.columns)
+test_train = pd.DataFrame(index = copy_train.index, columns = ['dur', 'proto', 'state', 'service'])
 
-discretization = pd.DataFrame(index = train_data.index, columns = train_data.columns)
-#print(discretization)
+number_train = raw_train_data.drop(['id', 'proto', 'state', 'service', 'attack_cat', 'label'], axis=1)
 
-for feature in train_data.columns:
-	temp = train_data[feature].as_matrix()
-	discretization[feature] = pd.cut(temp, 10)
+temp = number_train['dur'].as_matrix()
+test_train['dur'] = pd.cut(temp, 10)
+
+for feature in number_train.columns:
+    temp = number_train[feature].as_matrix()
+    cat_train[feature] = pd.cut(temp, 10)
+
+cat_train["proto"] = raw_train_data["proto"]
+cat_train["service"] = raw_train_data["service"]
+cat_train["state"] = raw_train_data["state"]
+
+test_train["proto"] = raw_train_data["proto"]
+test_train["service"] = raw_train_data["service"]
+test_train["state"] = raw_train_data["state"]
+
+#print(cat_train.columns)
+#print(cat_train.shape)
+#print(cat_train.values[0])
+#print(cat_train.values[1])
+#print(cat_train.values[2])
+
+print(test_train.columns)
+print(test_train.shape)
+#print(test_train.values[0])
+#print(test_train.values[1])
+#print(test_train.values[2])
 
 
-print(discretization)
 
 
+def subsets(arr):
+    """ Returns non empty subsets of arr"""
+    return chain(*[combinations(arr, i + 1) for i, a in enumerate(arr)])
+
+
+def returnItemsWithMinSupport(itemSet, transactionList, minSupport, freqSet):
+    """calculates the support for items in the itemSet and returns a subset of the itemSet each of whose elements satisfies the minimum support"""
+    _itemSet = set()
+    localSet = defaultdict(int)
+
+    for item in itemSet:
+        for transaction in transactionList:
+            if item.issubset(transaction):
+                freqSet[item] += 1
+                localSet[item] += 1
+
+    for item, count in localSet.items():
+        support = float(count)/len(transactionList)
+
+        if support >= minSupport:
+            _itemSet.add(item)
+
+    return _itemSet
+
+
+def joinSet(itemSet, length):
+    """Join a set with itself and returns the n-element itemsets"""
+    return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
+
+
+def getItemSetTransactionList(data_iterator):
+    transactionList = list()
+    itemSet = set()
+    for record in data_iterator:
+        transaction = frozenset(record)
+        transactionList.append(transaction)
+        for item in transaction:
+            itemSet.add(frozenset([item]))
+    return itemSet, transactionList
+
+
+def runApriori(data_iter, minSupport, minConfidence):
+    itemSet, transactionList = getItemSetTransactionList(data_iter)
+
+    freqSet = defaultdict(int)
+    largeSet = dict()
+    assocRules = dict()
+
+    oneCSet = returnItemsWithMinSupport(itemSet, transactionList, minSupport, freqSet)
+
+    currentLSet = oneCSet
+    k = 2
+    while(currentLSet != set([])):
+        largeSet[k-1] = currentLSet
+        currentLSet = joinSet(currentLSet, k)
+        currentCSet = returnItemsWithMinSupport(currentLSet, transactionList, minSupport, freqSet)
+        currentLSet = currentCSet
+        k = k + 1
+
+    def getSupport(item):
+        """local function which Returns the support of an item"""
+        return float(freqSet[item])/len(transactionList)
+
+    toRetItems = []
+    for key, value in largeSet.items():
+        toRetItems.extend([(tuple(item), getSupport(item)) for item in value])
+
+    toRetRules = []
+    for key, value in largeSet.items()[1:]:
+        for item in value:
+            _subsets = map(frozenset, [x for x in subsets(item)])
+            for element in _subsets:
+                remain = item.difference(element)
+                if len(remain) > 0:
+                    confidence = getSupport(item)/getSupport(element)
+                    if confidence >= minConfidence:
+                        toRetRules.append(((tuple(element), tuple(remain)), confidence))
+    return toRetItems, toRetRules
+
+
+def printResults(items, rules):
+    """prints the generated itemsets sorted by support and the confidence rules sorted by confidence"""
+    for item, support in sorted(items, key=lambda (item, support): support):
+        print "item: %s , %.3f" % (str(item), support)
+    print "\n------------------------ RULES:"
+    for rule, confidence in sorted(rules, key=lambda (rule, confidence): confidence):
+        pre, post = rule
+        print "Rule: %s ==> %s , %.3f" % (str(pre), str(post), confidence)
+
+
+def dataFromFile():
+    """Function which reads from the file and yields a generator"""
+    for i in range(0, 10):
+        print(copy_train.values[i])
+        record = frozenset(copy_train.values[i])
+        yield record
+
+
+if __name__ == "__main__":
+
+    optparser = OptionParser()
+    optparser.add_option('-s', '--minSupport', dest='minS', help='minimum support value', default=0.15, type='float')
+    optparser.add_option('-c', '--minConfidence', dest='minC', help='minimum confidence value', default=0.6, type='float')
+    
+    (options, args) = optparser.parse_args()
+
+    inFile = None
+    inFile = dataFromFile()
+
+    minSupport = options.minS
+    minConfidence = options.minC
+
+    items, rules = runApriori(inFile, minSupport, minConfidence)
+
+    printResults(items, rules)
