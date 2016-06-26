a06dc5e4a8dd09382baa1d2fc0997e8d7a02810c
r03921081 <a98703003@gmail.com>
2016-06-26 18:40:51 +0800

    Rule

--- max_sup_freq_rule.py
+++ max_sup_freq_rule.py
@@ -8,6 +8,19 @@ feature = 5
 
 attack_table = ["Normal", "Reconnaissance", "Backdoor", "DoS", "Exploits", "Analysis", "Fuzzers", "Worms", "Shellcode", "Generic"]
 
+fNormal = {}
+fReconnaissance = {}
+fBackdoor = {}
+fDoS = {}
+fExploits = {}
+fAnalysis = {}
+fFuzzers = {}
+fWorms = {}
+fShellcode = {}
+fGeneric = {}
+
+attack_feature = {"Normal": fNormal, "Reconnaissance": fReconnaissance, "Backdoor": fBackdoor, "DoS": fDoS, "Exploits": fExploits, "Analysis": fAnalysis, "Fuzzers": fFuzzers, "Worms": fWorms, "Shellcode": fShellcode, "Generic": fGeneric}
+
 raw_train_data = pd.read_csv("unsw/UNSW_Discretize.csv", delimiter=',', encoding="utf-8-sig")
 #print(raw_train_data.columns)
 print(raw_train_data.shape)
@@ -18,13 +31,10 @@ for feature in raw_train_data.columns:
 	for item in raw_train_data[feature].values:
 		if item not in category:
 			category[item] = feature
-#print(category)
 
 train_data = raw_train_data.sample(frac=0.1, replace=True)
 #print(train_data)
 transactions = []
-
-
 
 for label in attack_table:
 	print(label)
@@ -38,6 +48,8 @@ for label in attack_table:
 	if minsup < 500:
 		minsup = 500
 	#print(minsup)
+	if label == "Generic":
+		minsup = 3000
 
 	label_item = {}
 	new_itemset = []
@@ -50,23 +62,36 @@ for label in attack_table:
 		trans_item = []
 		for token in itemset:
 			trans_item.append(category[token])
-		str_itemset = ', '.join(trans_item)
+		str_itemset = ','.join(trans_item)
 		label_item[str_itemset] = sup
 		if sup > maxsup:
 			maxsup = sup
 	new_itemset = sorted(label_item.items(), key=operator.itemgetter(1), reverse=True)
 	
+	max_rule = []	
+
 	for rule in new_itemset:
 		if int(rule[1]) == maxsup:
 			num = num + 1
-			print(rule)
+			max_rule.append(rule[0])
 		elif num < 10:
 			num = num + 1
-			print(rule)
-		#if num < 10:
-		#	num = num + 1
-		#	print(rule)
-		#else:
-		#	break
-
+			max_rule.append(rule[0])
+	for token in max_rule:
+		for take_feature in token.split(","):
+			#print(take_feature)
+			if take_feature not in attack_feature[label]:
+				attack_feature[label][take_feature] = 0
+			else:
+				attack_feature[label][take_feature] = int(attack_feature[label][take_feature]) + 1
+	print(attack_feature[label])
 	print("Count " + str(count))
+
+print(attack_feature)
+
+for print_rule in attack_feature:
+	print("")
+	print(print_rule)
+	choose_feature = sorted(attack_feature[print_rule].items(), key=operator.itemgetter(1), reverse=True)
+	print(choose_feature)
+
