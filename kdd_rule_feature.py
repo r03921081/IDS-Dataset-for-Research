aa352ea0ef25e1255ee03d1252aa10948c36c89d
r03921081 <a98703003@gmail.com>
2016-07-07 15:08:38 +0800

    KDD

--- kdd_rule_feature.py
+++ kdd_rule_feature.py
@@ -7,14 +7,23 @@ import random
 minsup = 5000
 feature = 5
 
-attack_table = ["normal", "anomaly"]
+attack_table = ["normal", "back", "land", "neptune", "pod", "smurf", "teardrop", "buffer_overflow", "loadmodule", "perl", "rootkit", "ftp_write", "guess_passwd", "imap", "multihop", "phf", "spy", "warezlient", "warezmaster", "ipsweep", "nmap", "satan", "portsweep"]
 
 fNormal = {}
+fDos = {}
+fU2R = {}
+fR2L = {}
+fProbe = {}
 fAbnormal = {}
 
-attack_feature = {"normal": fNormal, "anomaly": fAbnormal}
+dos_type = ["back", "land", "neptune", "pod", "smurf", "teardrop"]
+u2r_type = ["buffer_overflow", "loadmodule", "perl", "rootkit"]
+r2l_type = ["ftp_write", "guess_passwd", "imap", "multihop", "phf", "spy", "warezlient", "warezmaster"]
+probe_type = ["ipsweep", "nmap", "satan", "portsweep"]
 
-raw_train_data = pd.read_csv("nsl_kdd/KDD_Train_All_Discretization.csv", delimiter=',', encoding="utf-8-sig")
+attack_feature = {"normal": fNormal, "dos": fDos, "u2r": fU2R, "r2l": fR2L, "probe": fProbe, "anomaly": fAbnormal}
+
+raw_train_data = pd.read_csv("nsl_kdd/New_KDD_Train_Discretization_freq.csv", delimiter=',', encoding="utf-8-sig")
 
 #raw_train_data = raw_data.drop(["ct_dst_sport_ltm"], axis=1)
 
@@ -28,9 +37,9 @@ for feature in raw_train_data.columns:
 		if item not in category:
 			category[item] = feature
 
-sample_num = int(raw_train_data.shape[0] * 0.2)
-train_data = raw_train_data.loc[random.sample(list(raw_train_data.index), sample_num)]
-
+#sample_num = int(raw_train_data.shape[0] * 0.5)
+#train_data = raw_train_data.loc[random.sample(list(raw_train_data.index), sample_num)]
+train_data = raw_train_data
 #train_data = raw_train_data.sample(frac=0.2, replace=True)
 transactions = []
 
@@ -42,7 +51,22 @@ for label in attack_table:
 	np_data = data.as_matrix()
 	transactions = np_data.tolist()
 
+	if label in dos_type:
+		Attack_label = "dos"
+	elif label in u2r_type:
+		Attack_label = "u2r"
+	elif label in r2l_type:
+		Attack_label = "r2l"
+	elif label in probe_type:
+		Attack_label = "probe"
+	else:
+		Attack_label = "normal"
+
+	print Attack_label
+
 	minsup = data.shape[0]
+	if minsup < 200:
+		minsup = 200
 	#minsup = int(minsup * 9 / 10)	
 
 	label_item = {}
@@ -66,19 +90,26 @@ for label in attack_table:
 
 	for rule in new_itemset:
 		if int(rule[1]) == maxsup:
+			#print(rule[0])
 			num = num + 1
 			max_rule.append(rule[0])
 		elif num < 10:
+			#print(rule[0])
 			num = num + 1
 			max_rule.append(rule[0])
 	for token in max_rule:
 		for take_feature in token.split(","):
 			#print(take_feature)
-			if take_feature not in attack_feature[label]:
-				attack_feature[label][take_feature] = 0
+			if take_feature not in attack_feature[Attack_label]:
+				attack_feature[Attack_label][take_feature] = 0
 			else:
-				attack_feature[label][take_feature] = int(attack_feature[label][take_feature]) + 1
-	print(attack_feature[label])
+				attack_feature[Attack_label][take_feature] = int(attack_feature[Attack_label][take_feature]) + 1
+			if label != "normal":
+				if take_feature not in attack_feature["anomaly"]:
+					attack_feature["anomaly"][take_feature] = 0
+				else:
+					attack_feature["anomaly"][take_feature] = int(attack_feature["anomaly"][take_feature]) + 1
+	#print(attack_feature[Attack_label])
 	print("Count " + str(count))
 
 print(attack_feature)
--- kdd_rule_pred.py
+++ kdd_rule_pred.py
@@ -7,17 +7,62 @@ from sklearn.feature_extraction import DictVectorizer as dv
 from sklearn.cross_validation import train_test_split
 from sklearn import linear_model
 
-feature = ["dst_host_count", "src_bytes"]
+feature = ["num_access_files", "num_file_creations", "service", "num_compromised", "flag", "hot", "num_root", "duration", "num_failed_logins", "protocol_type"]
+
+proto_table = {"tcp": 0, "udp": 1, "icmp": 2}
+service_table = {"ftp_data": 0, "other": 1, "private": 2, "http": 3, "remote_job": 4, "name": 5, "netbios_ns": 6, "eco_i": 7, "mtp": 8, "telnet": 9, "finger": 10, "domain_u": 11, "supdup": 12, "uucp_path": 13, "Z39_50": 14, "smtp": 15, "csnet_ns": 16, "uucp": 17, "netbios_dgm": 18, "urp_i": 19, "auth": 20, "domain": 21, "ftp": 22, "bgp": 23, "ldap": 24, "ecr_i": 25, "gopher": 26, "vmnet": 27, "systat": 28, "http_443": 29, "efs": 30, "whois": 31, "imap4": 32, "iso_tsap": 33, "echo": 34, "klogin": 35, "link": 36, "sunrpc": 37, "login": 38, "kshell": 39, "sql_net": 40, "time": 41, "hostnames": 42, "exec": 43, "ntp_u": 44, "discard": 45, "nntp": 46, "courier": 47, "ctf": 48, "ssh": 49, "daytime": 50, "shell": 51, "netstat": 52, "pop_3": 53, "nnsp": 54, "IRC": 55, "pop_2": 56, "printer": 57, "tim_i": 58, "pm_dump": 59, "red_i": 60, "netbios_ssn": 61, "rje": 62, "X11": 63, "urh_i": 64, "http_8001": 65, "aoi": 66, "http_2784": 67, "tffp_u": 68, "harvest": 69}
+flag_table = {"SF": 0, "S0": 1, "REJ": 2, "RSTR": 3, "SH": 4, "RSTO": 5, "S1": 6, "RSTOS0": 7, "S3": 8, "S2": 9, "OTH": 10}
 
 raw_train_data = pd.read_csv("nsl_kdd/KDD_Train.csv", delimiter=',', encoding="utf-8-sig")
 raw_test_data = pd.read_csv("nsl_kdd/KDD_Test.csv", delimiter=',', encoding="utf-8-sig")
 
+cat_train = pd.DataFrame(index = raw_train_data.index, columns = ["protocol_type", "service", "flag"])
+cat_test = pd.DataFrame(index = raw_test_data.index, columns = ["protocol_type", "service", "flag"])
+
 train_data = pd.DataFrame(index = raw_train_data.index, columns = feature)
 test_data = pd.DataFrame(index = raw_test_data.index, columns = feature)
 
+for i in range(raw_train_data.shape[0]):
+	if raw_train_data["protocol_type"][i] in proto_table:
+		cat_train["protocol_type"][i] = proto_table[raw_train_data["protocol_type"][i]]
+	else:
+		cat_train["protocol_type"][i] = 0
+	if raw_train_data["service"][i] in service_table:
+		cat_train["service"][i] = service_table[raw_train_data["service"][i]]
+	else:
+		cat_train["service"][i] = 0
+	if raw_train_data["flag"][i] in flag_table:
+		cat_train["flag"][i] = flag_table[raw_train_data["flag"][i]]
+	else:
+		cat_train["flag"][i] = 0
+
+for i in range(raw_test_data.shape[0]):
+	if raw_test_data["protocol_type"][i] in proto_table:
+		cat_test["protocol_type"][i] = proto_table[raw_test_data["protocol_type"][i]]
+	else:
+		cat_test["protocol_type"][i] = 0
+	if raw_test_data["service"][i] in service_table:
+		cat_test["service"][i] = service_table[raw_test_data["service"][i]]
+	else:
+		cat_test["service"][i] = 0
+	if raw_test_data["flag"][i] in flag_table:
+		cat_test["flag"][i] = flag_table[raw_test_data["flag"][i]]
+	else:
+		cat_test["flag"][i] = 0
+
 for item in feature:
-	train_data[item] = raw_train_data[item]
-	test_data[item] = raw_test_data[item]
+	if item == "protocol_type":
+		train_data[item] = cat_train[item]
+		test_data[item] = cat_test[item]
+	elif item == "service":
+		train_data[item] = cat_train[item]
+		test_data[item] = cat_test[item]
+	elif item == "flag":
+		train_data[item] = cat_train[item]
+		test_data[item] = cat_test[item]
+	else:
+		train_data[item] = raw_train_data[item]
+		test_data[item] = raw_test_data[item]
 
 train_target = raw_train_data['class']
 test_target = raw_test_data['class']
